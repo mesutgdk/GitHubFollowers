1013a23cba75d57a563865570f4fcb551e6dc9f7
mesutgdk <mesuttgedik@gmail.com>
2024-03-07 15:34:43 +0300

    feat: custombutton

---
+++ GitHubFollowers/GitHubFollowers/Custom Views/CustomButton.swift
@@ -0,0 +1,23 @@
+//
+//  CustomButton.swift
+//  GitHubFollowers
+//
+//  Created by Mesut Gedik on 7.03.2024.
+//
+
+import UIKit
+
+class CustomButton: UIButton{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(backgroundColor: UIColor, title: String) {
+        self.init(frame: .zero) 
+        self.backgroundColor =
+    }
+}
