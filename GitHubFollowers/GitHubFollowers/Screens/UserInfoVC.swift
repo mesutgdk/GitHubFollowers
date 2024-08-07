3aa78d4c8e41696d843910d73169431e75f4a156
mesutgdk <mesuttgedik@gmail.com>
2024-08-07 09:49:41 +0300

    feat: select item navigate to useringfovc

--- GitHubFollowers/GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/GitHubFollowers/Screens/UserInfoVC.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class UserInfoVC: UIViewController {
+final class UserInfoVC: UIViewController {
+    
+    var userName : Follower
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setup()
+        layout()
+        createDoneButton()
     }
     
+    
+    private func setup(){
+        view.backgroundColor = .systemBackground
 
+    }
+    
+    private func layout(){
+        
+    }
+    
+    
+    private func createDoneButton(){
+        navigationController?.navigationBar.isHidden = false
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+
+    @objc func dismissVC(){
+        dismiss(animated: true)
+    }
 
 }
