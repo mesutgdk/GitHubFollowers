63fa4e334545257f81ac71f641720c89300a77cf
mesutgdk <mesuttgedik@gmail.com>
2024-08-22 10:03:25 +0300

    feat: customuserinfoheader vc is created to show profile and bio

---
+++ GitHubFollowers/GitHubFollowers/Custom Views/ViewControllers/CustomUserInfoHeaderVC.swift
@@ -0,0 +1,18 @@
+//
+//  CustomUserInfoHeaderVC.swift
+//  GitHubFollowers
+//
+//  Created by Mesut Gedik on 22.08.2024.
+//
+
+import UIKit
+
+class CustomUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
