204c84925917132885ba0a080ce734924113d2ab
mesutgdk <mesuttgedik@gmail.com>
2024-09-02 15:32:48 +0300

    feat: CustomUserInfoHeaderVC layout

--- GitHubFollowers/GitHubFollowers/Custom Views/ViewControllers/CustomUserInfoHeaderVC.swift
+++ GitHubFollowers/GitHubFollowers/Custom Views/ViewControllers/CustomUserInfoHeaderVC.swift
@@ -12,15 +12,49 @@ final class CustomUserInfoHeaderVC: UIViewController {
     let avatarImageView     = CustomImageView(frame: .zero)
     let usernameLabel       = CustomTitleLabel(textAlignment: .left, fontSize: 34)
     let nameLabel           = CustomSecondaryTitleLabel(fontSize: 18)
-    let locationImageView   = UIImageView()
     let locationLabel       = CustomSecondaryTitleLabel(fontSize: 18)
     let bioLabel            = CustomBodyLabel(textAlignment: .center, numberOfLines: 20)
+    let locationImageView   = {
+        let locationImageView = UIImageView()
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
+        return locationImageView
+    }()
     
     var user : User?
     
+    init(user: User?) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        layout()
 
+    }
+    
+    private func setup(){
+        view.addSubviews(
+            avatarImageView,
+            usernameLabel,
+            nameLabel,
+            bioLabel,
+            locationLabel,
+            locationImageView
+            
+        )
+    }
+    
+    private func layout(){
+        let padding             :CGFloat        = 20
+        let textImagePadding    :CGFloat        = 12
+        
+        
     }
     
 }
