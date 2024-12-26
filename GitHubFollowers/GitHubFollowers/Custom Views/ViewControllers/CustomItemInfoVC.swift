752461ea10fa482e3cf89528a2531a7d31748997
mesutgdk <mesuttgedik@gmail.com>
2024-12-26 11:06:38 +0300

    feat: iteminfo vc

---
+++ GitHubFollowers/GitHubFollowers/Custom Views/ViewControllers/CustomItemInfoVC.swift
@@ -0,0 +1,19 @@
+//
+//  CustomItemInfoVC.swift
+//  GitHubFollowers
+//
+//  Created by Mesut Gedik on 26.12.2024.
+//
+
+import UIKit
+
+class CustomItemInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
