6ce8505a2f2aeedb3969d552fe10065f2e54662c
mesutgdk <mesuttgedik@gmail.com>
2025-01-31 08:27:09 +0300

    feat: persistencemanager to retrive favorites

---
+++ GitHubFollowers/GitHubFollowers/Custom Views/Cell/FavoriteTVCell.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteTVCell.swift
+//  GitHubFollowers
+//
+//  Created by Mesut Gedik on 30.01.2025.
+//
+
+import UIKit
+
+class FavoriteTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
