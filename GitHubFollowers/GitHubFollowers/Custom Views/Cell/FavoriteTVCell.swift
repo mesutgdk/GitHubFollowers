c8d143b8c468366044c4f6765f941cb777c5e55b
mesutgdk <mesuttgedik@gmail.com>
2025-01-31 10:35:30 +0300

    favorireCell Layout

--- GitHubFollowers/GitHubFollowers/Custom Views/Cell/FavoriteTVCell.swift
+++ GitHubFollowers/GitHubFollowers/Custom Views/Cell/FavoriteTVCell.swift
@@ -25,21 +25,36 @@ class FavoriteTVCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func configure(favorite: Follower){
+        usernameLabel.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
+    
     private func setup() {
         addSubviews(avatarImageView, usernameLabel)
         
-        accessoryType             = .disclosureIndicator
+        accessoryType                   = .disclosureIndicator
         
     }
     
     private func layout() {
-        let padding:CGFloat       = 12
+        let padding:CGFloat             = 12
+        let avatarSqureSide:CGFloat     = 60
+        let usernameHeight:CGFloat      = 40
         
         NSLayoutConstraint.activate([
             avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
-            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
-            avatarImageView.widthAnchor.constraint(equalTo: avatarImageView.heightAnchor)
+            avatarImageView.heightAnchor.constraint(equalToConstant: avatarSqureSide),
+            avatarImageView.widthAnchor.constraint(equalToConstant: avatarSqureSide)
+        ])
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: padding*2),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: usernameHeight)
+            
         ])
         
     }
