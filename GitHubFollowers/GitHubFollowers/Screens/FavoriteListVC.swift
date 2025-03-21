5570a8b91c9703464d2c05668cef5dfae3727916
mesutgdk <mesuttgedik@gmail.com>
2025-03-21 10:11:35 +0300

    refactor: spacing

--- GitHubFollowers/GitHubFollowers/Screens/FavoriteListVC.swift
+++ GitHubFollowers/GitHubFollowers/Screens/FavoriteListVC.swift
@@ -23,6 +23,8 @@ final class FavoriteListVC: UIViewController {
         // viewDidLoad pulls one time, if u change screen it cant refresh, so viewDidAppear is a good solution.
         getFavorites()
     }
+    
+    
     private func setup(){
         //configureVC
         view.backgroundColor    = .systemBackground
@@ -35,9 +37,11 @@ final class FavoriteListVC: UIViewController {
         
     }
     
+    
     private func layout(){
         
     }
+    
     
     private func configureTableView(){
         tableView.frame         = view.bounds
@@ -50,31 +54,40 @@ final class FavoriteListVC: UIViewController {
         tableView.register(FavoriteTVCell.self, forCellReuseIdentifier: FavoriteTVCell.cellIdentifier)
     }
     
+    
     func getFavorites(){
         PersistenceManager.retriveFavorites { [weak self] result in
             guard let self = self else {return}
             switch result {
             case .success(let favorites):
-
-                if favorites.isEmpty {
-                    self.showEmptyStateView(with: "No Favorites?\nAdd some on the followers!", in: self.view)
-                } else {
-                    self.favorites = favorites
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                        self.view.bringSubviewToFront(self.tableView)
-                    }
-                }
+                updateUI(with: favorites)
+                
             case .failure(let error):
                 self.presentCustomAlertOnMainThread(title: "Something went wrong", message: error.errorDescription, buttonTitle: "Ok")
             }
         }
     }
+    
+    
+    func updateUI(with favorites: [Follower]){
+        
+        if favorites.isEmpty {
+            self.showEmptyStateView(with: "No Favorites?\nAdd some on the followers!", in: self.view)
+        } else {
+            self.favorites = favorites
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.view.bringSubviewToFront(self.tableView)
+            }
+        }
+    }
 }
+// MARK: - TableViewDataSource Delegate
 extension FavoriteListVC: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         favorites.count
     }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteTVCell.cellIdentifier) as! FavoriteTVCell
@@ -83,6 +96,7 @@ extension FavoriteListVC: UITableViewDataSource, UITableViewDelegate{
         return cell
     }
     
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let favorite            = favorites[indexPath.row]
         let destinationVC       = FollowerListVC(username: favorite.login)
@@ -90,6 +104,7 @@ extension FavoriteListVC: UITableViewDataSource, UITableViewDelegate{
         navigationController?.pushViewController(destinationVC, animated: true)
         
     }
+    
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
